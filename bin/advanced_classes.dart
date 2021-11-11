93b2739fd480c9eef49f05690831c0668b3fc0de
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-11 08:28:07 +0200

    interfaces

--- bin/advanced_classes.dart
+++ bin/advanced_classes.dart
@@ -1,33 +1,57 @@
 void main(List<String> arguments) {
-  final platypus = Platypus();
-  print(platypus.isAlive);
-  platypus.eat();
-  platypus.move();
-  platypus.layEggs();
-  print(platypus);
+  final DataRepository repository = DataRepository();
+  final temperature = repository.fetchTemperature('Harare');
+  print(temperature);
+
+  final someClass = SomeClass();
+  print(someClass.myField);
+  someClass.myMethod();
+
+  final someOtherClass = SomeOtherClass();
+  print(someOtherClass.myField);
+  someOtherClass.myMethod();
+
+  final Bottle bottle = Bottle();
+  bottle.open();
 }
 
-abstract class Animal {
-  bool isAlive = true;
-  void eat();
-  void move();
-
-  @override
-  String toString() => "I'm a $runtimeType";
+abstract class DataRepository {
+  factory DataRepository() => FakeWebServer();
+  double? fetchTemperature(String city);
 }
 
-class Platypus extends Animal {
+class FakeWebServer implements DataRepository {
   @override
-  void eat() {
-    print('Munch munch');
+  double? fetchTemperature(String city) {
+    return 42.0;
   }
+}
+
+class AnotherClass {
+  int myField = 67;
+  void myMethod() => print(myField);
+}
+
+class SomeClass extends AnotherClass {}
+
+class SomeOtherClass implements AnotherClass {
+  @override
+  int myField = 0;
 
   @override
-  void move() {
-    print('Glide glide');
+  void myMethod() {
+    print('Hello');
   }
+}
 
-  void layEggs() {
-    print('Plop plop');
+abstract class Bottle {
+  factory Bottle() => SodaBottle();
+  void open();
+}
+
+class SodaBottle implements Bottle {
+  @override
+  void open() {
+    print('Fizz fizz');
   }
 }

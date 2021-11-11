5100ded892c991ed44adedd94ad88360c6354e2e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-11 08:00:21 +0200

    abstract classes

--- bin/advanced_classes.dart
+++ bin/advanced_classes.dart
@@ -1,110 +1,33 @@
 void main(List<String> arguments) {
-  final jon = Person('Jon', 'Snow');
-  final jane = Student('Jane', 'Snow');
-  print(jon.fullName);
-  print(jane.fullName);
-  final historyGrade = Grade.B;
-  jane.grades.add(historyGrade);
-  print(jane.grades);
-
-  final child = SomeChild();
-  child.doSomeWork();
-
-  final jessie = SchoolBandMember('Jessie', 'Jones');
-  final marty = StudentAthlete('Marty', 'McFly');
-  print(jessie);
-  print(marty);
-
-  print(jessie is Object);
-  print(jessie is Person);
-  print(jessie is Student);
-  print(jessie is SchoolBandMember);
-  print(jessie is! StudentAthlete);
-
-  final fruit = Fruit('Orange');
-  fruit.describeColor();
-
-  final melon = Melon('Green');
-  melon.describeColor();
-
-  final watermelon = Watermelon('Yellow');
-  watermelon.describeColor();
-
-  final canta = Cantaloupe('Blue');
-  canta.describeColor();
+  final platypus = Platypus();
+  print(platypus.isAlive);
+  platypus.eat();
+  platypus.move();
+  platypus.layEggs();
+  print(platypus);
 }
 
-enum Grade { A, B, C, D, F }
-
-class Person {
-  Person(this.givenName, this.surname);
-
-  String givenName;
-  String surname;
-  String get fullName => '$givenName $surname';
+abstract class Animal {
+  bool isAlive = true;
+  void eat();
+  void move();
 
   @override
-  String toString() => fullName;
+  String toString() => "I'm a $runtimeType";
 }
 
-class Student extends Person {
-  Student(String givenName, String surname) : super(givenName, surname);
-
-  var grades = <Grade>[];
+class Platypus extends Animal {
+  @override
+  void eat() {
+    print('Munch munch');
+  }
 
   @override
-  String get fullName => '$surname, $givenName';
-}
-
-class SchoolBandMember extends Student {
-  SchoolBandMember(String givenName, String surname)
-      : super(givenName, surname);
-
-  static const minimumPracticeTime = 2;
-}
-
-class StudentAthlete extends Student {
-  StudentAthlete(String givenName, String surname) : super(givenName, surname);
-
-  bool get isEligible => grades.every((grade) => grade != Grade.F);
-}
-
-class SomeParent {
-  void doSomeWork() {
-    print('parent working');
+  void move() {
+    print('Glide glide');
   }
-}
 
-class SomeChild extends SomeParent {
-  @override
-  void doSomeWork() {
-    super.doSomeWork();
-    print('child doing some other work');
+  void layEggs() {
+    print('Plop plop');
   }
-}
-
-class Fruit {
-  Fruit(this.color);
-  final String color;
-
-  void describeColor() {
-    print('Fruit of color --> $color');
-  }
-}
-
-class Melon extends Fruit {
-  Melon(String color) : super(color);
-}
-
-class Watermelon extends Melon {
-  Watermelon(String color) : super(color);
-
-  @override
-  void describeColor() {
-    print('Watermelon of color --> $color');
-  }
-}
-
-class Cantaloupe extends Melon {
-  Cantaloupe(String color) : super(color);
 }
